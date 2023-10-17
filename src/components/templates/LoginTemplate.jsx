946161e06ec32e8fac4225435e0c0a98fc3a7877
adrianhh1426 <adrianhh1426@gmail.com>
2023-10-17 16:38:49 -0600

    agregando imagen flotando

--- src/components/templates/LoginTemplate.jsx
+++ src/components/templates/LoginTemplate.jsx
@@ -5,10 +5,10 @@ import { v } from '../../styles/variables';
 const LoginTemplate = () => {
   return (
     <Container>
-      <div>
-        <span>Version 1.0</span>
-        <div>
-          <img src="" alt="" />
+      <div className='contentCard'>
+        <span className='version'>Version 1.0</span>
+        <div className='contentImg'>
+          <img src={v.logo} alt="" />
         </div>
 
         <Title>Control de gastos</Title>
@@ -24,7 +24,53 @@ const LoginTemplate = () => {
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  background-image: url(${v.imagenfondo});
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: rgba(255,255,255,0.87);
+  text-align: center;
+
+  .contentCard{
+    background-color: #131313;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    padding: 20px;
+    box-shadow: 8px 5px 18px 3px rgba(0,0,0,0.35);
+
+    .version{
+      color: #727272;
+      text-align: start;
+    }
+
+    .contentImg{
+      img{
+        max-width: 60%;
+        animation: flotar 1.5s ease-in-out infinite alternate;
+      }
+    }
+  }
+
+  @keyframes flotar {
+    0%{
+      transform: translate(0,0px);
+    }
+
+    50%{
+      transform: translate(0,15px);
+    }
+
+    100%{
+      transform: translate(0,-0px);
+    }
+  }
+`;
 
 const Title = styled.span`
   font-size: 5rem;
