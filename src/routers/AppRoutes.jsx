c59b9da08c4f557d6ac5f10975e10aeb3f295c55
adrianhh1426 <adrianhh1426@gmail.com>
2023-10-18 17:40:41 -0600

    agregando rutas protegidas

--- src/routers/AppRoutes.jsx
+++ src/routers/AppRoutes.jsx
@@ -1,13 +1,19 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
 import HomePage from '../pages/HomePage';
+import ProtectedRoute from './ProtectedRoute';
+import { UserAuthContext } from '../context/AuthContext';
 
 const AppRoutes = () => {
+  const { user } = UserAuthContext();
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/home' element={<HomePage />} />
-        <Route path='/' element={<LoginPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route element={<ProtectedRoute user={user} redirectTo='/login' />}>
+          <Route path='/' element={<HomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/routers/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import { Navigate, Outlet } from 'react-router-dom';
+
+const ProtectedRoute = (props) => {
+  const { user, redirectTo, children } = props;
+
+  if (user === null) {
+    <Navigate replace to={redirectTo} />
+  }
+
+  return children ? children : <Outlet />;
+};
+
+ProtectedRoute.propTypes = {
+  user: PropTypes.any,
+  redirectTo: PropTypes.string,
+  children: PropTypes.node
+}
+
+export default ProtectedRoute;
