0ac90554d277ff313857c5c2f9fdf71c4bd90675
adrianhh1426 <adrianhh1426@gmail.com>
2023-10-23 15:57:41 -0600

    insert ususarios

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,12 +1,25 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { supabase } from '../supabase/supabase.config';
+import { addUser } from '../supabase/usuariosService';
 
 const AuthContext = createContext();
 
 export const AuthContextProvider = (props) => {
   const { children } = props;
   const [user, setUser] = useState([]);
+
+  const insertUsuario = async (dataProvider, idAuthSupabase) => {
+    const { name, picture } = dataProvider;
+
+    const user = {
+      nombres: name,
+      foto: picture,
+      idauth_supabase: idAuthSupabase
+    };
+
+    await addUser(user);
+  };
 
   useEffect(() => {
     const { data: authListener } = supabase.auth.onAuthStateChange(
@@ -16,6 +29,7 @@ export const AuthContextProvider = (props) => {
           setUser(null);
         } else {
           setUser(session?.user.user_metadata);
+          insertUsuario(session?.user.user_metadata, session?.user.id);
         }
       }
 
