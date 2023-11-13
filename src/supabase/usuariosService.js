61963daece08b039726bc0d8a2c127718a7c7a94
adrianhh1426 <adrianhh1426@gmail.com>
2023-11-13 17:16:11 -0600

    creando metodo para extraer data de usuario logueado

--- src/supabase/usuariosService.js
+++ src/supabase/usuariosService.js
@@ -1,3 +1,4 @@
+import { getIdAuthSupabase } from "./session";
 import { supabase } from "./supabase.config";
 
 export const addUser = async (user) => {
@@ -7,5 +8,22 @@ export const addUser = async (user) => {
     return data;
   } catch (error) {
     return error;
+  }
+};
+
+export const mostrarUsuarios = async () => {
+  try {
+    const idAuthSupabase = await getIdAuthSupabase();
+
+    const { data } = await supabase
+      .from("usuarios")
+      .select()
+      .eq("idauth_supabase", idAuthSupabase);
+
+    if (data) {
+      return data[0];
+    }
+  } catch (error) {
+    // alert(error.error_description || error.message + "MostrarUsuarios");
   }
 };
