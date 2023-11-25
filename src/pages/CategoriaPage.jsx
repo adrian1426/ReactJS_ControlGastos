2d5200cff73f74342d606f3b1071dc0813ea6237
adrianhh1426 <adrianhh1426@gmail.com>
2023-11-25 17:16:38 -0600

    estrayendo datos de categorias en susbase

--- src/pages/CategoriaPage.jsx
+++ src/pages/CategoriaPage.jsx
@@ -1,7 +1,25 @@
 import styled from "styled-components";
 import CategoriaTemplate from "../components/templates/CategoriaTemplate";
+import { useCategoriasStore } from "../store/CategoriasStore";
+import { useQuery } from "@tanstack/react-query";
+import { useUserStore } from "../store/UserStore";
 
 const CategoriaPage = () => {
+  const { dataUsuarios } = useUserStore();
+  const { datacategoria, mostrarCategorias } = useCategoriasStore();
+  const { isLoading, error } = useQuery({
+    queryKey: ['mostrar-categorias'],
+    queryFn: () => mostrarCategorias({ idusuario: dataUsuarios.id, tipo: "i" })
+  });
+
+  if (isLoading) {
+    return <h1>Cargando...</h1>;
+  }
+
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  }
+
   return (
     <Container>
       <CategoriaTemplate />
