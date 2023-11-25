afbdba0ed780b3c974da60303668abb2b5b86f28
adrianhh1426 <adrianhh1426@gmail.com>
2023-11-25 16:14:31 -0600

    store de categorias

---
+++ src/store/CategoriasStore.js
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+import { EditarCategorias, EliminarCategorias, InsertarCategorias, MostrarCategorias } from "../supabase/categoriasService";
+
+export const useCategoriasStore = create((set, get) => ({
+  datacategoria: [],
+  mostrarCategorias: async (p) => {
+    const response = await MostrarCategorias(p);
+    set({ datacategoria: response });
+    return response;
+  },
+  insertarCategorias: async (p) => {
+    await InsertarCategorias(p);
+    const { mostrarCategorias } = get();
+    set(mostrarCategorias(p));
+  },
+  eliminarCategoria: async (p) => {
+    await EliminarCategorias(p);
+    const { mostrarCategorias } = get();
+    set(mostrarCategorias(p));
+  },
+  editarCategoria: async (p) => {
+    await EditarCategorias(p);
+    const { mostrarCategorias } = get();
+    set(mostrarCategorias(p));
+  },
+}));
